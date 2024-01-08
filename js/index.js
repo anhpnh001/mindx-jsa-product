e9dbda945f9e2a91520075c35ddd40e5ca3fb6b5
Phan Ngọc Hoàng Anh <2156013115yen@ou.edu.vn>
2024-01-08 07:38:19 +0000

    Fix path

--- js/index.js
+++ js/index.js
@@ -159,10 +159,10 @@ topInformationPosts.forEach((post) => {
   const postElement = document.createElement('div')
   postElement.classList.add('post')
   postElement.innerHTML = `
-        <a href="/information.html" class="post__category">Thông tin</a>
-        <a href="/detail.html?id=${post.id}" class="post__title">${
-    post.title
-  }</a>
+        <a href="/mindx-jsa-product/information.html" class="post__category">Thông tin</a>
+        <a href="/mindx-jsa-product/detail.html?id=${
+          post.id
+        }" class="post__title">${post.title}</a>
         <p class="post__text">${post.content.slice(0, 100)}...</p>
     `
   information.appendChild(postElement)
